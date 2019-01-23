cff8328a59787cd176e6f5488dce6c79150d0f4f
Hamza <hamzayaghi5@gmail.com>
2019-01-23 11:40:09 +0200

    add two new sections

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  				items: 1,
+                responsiveClass: true,
+                rtl: true,
+                mouseDrag: true,
+                touchDrag: true,
+                loop:true,
+                autoplay:true,
+    			autoplayTimeout:2000,
+              
+            });
+});
+
+
